c95d96b43288b457ca6c55fe2193338fa36279cb
Koki Kawasaki <kawasaki.koki@tis.co.jp>
2016-09-05 09:02:32 +0000

    processing

--- p38.py
+++ p38.py
@@ -17,26 +17,6 @@ from p30 import load_text, make_MorphemeDict, make_DictList
 
 from p36 import count_ApearedWords
 
-def plot_BarGraph(words_counter):
-	# num of all apeared words
-	num_words = sum(words_counter.values())
-	# top 10 words most apeared
-	top10 = words_counter.most_common(10)
-
-	# x-axis
-	left = np.array(range(10))
-	# y-axis
-	height = np.array([top10[i][1] / num_words for i in range(10)])
-	plt.xlabel("Words")
-	plt.ylabel("Appearance frequency")
-	# ticks of x-axis
-	tick = [top10[i][0] for i in range(10)]
-	fp = FontProperties(fname=r'/root/NLP100/IPAexfont00301/ipaexm.ttf', size=14)
-	plt.xticks(left, tick, fontproperties = fp)
-	# plot
-	plt.bar(left, height, align="center")
-	# save as png
-	plt.savefig("Appearance_Frequency_of_words.png")
 
 def plot_Histgram(words_counter):
 	# num of all apeared words
@@ -46,7 +26,7 @@ def plot_Histgram(words_counter):
 	# freq = np.array([words_counter.values()[i] / num_words for i in range(len(words_counter))])
 	freq = list(sorted(words_counter.values()))
 	# print(freq)
-	plt.xlabel("Appearance frequency")
+	plt.xlabel("Appearance frequency!!!")
 	plt.ylabel("Frequency of appearance frequency")
 	# plot
 	# plt.hist(freq, bins = 50, normed = True)
